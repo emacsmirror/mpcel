d57d6bc8699e74aea1ff479ef0c010ec1e386a49
Rene Borchers <rmborchers@gmail.com>
2013-01-07 20:14:43 +0100

    Uncommented the function (mpcel-playlist-clear

--- mpcel.el
+++ mpcel.el
@@ -243,12 +243,12 @@
          (setq buffer-read-only t))
 )
 
-;; clear playlist
-;; (defun mpcel-playlist-clear ()
-;;   "Clear the mpd playlist"
-;;   (interactive)
-;;   (call-process mpcel-mpc-program nil nil nil "clear")
-;;   (message "the playlist is cleared"))
+;;clear playlist
+(defun mpcel-playlist-clear ()
+   "Clear the mpd playlist"
+   (interactive)
+   (call-process mpcel-mpc-program nil nil nil "clear")
+   (message "the playlist is cleared"))
 
 ;; add song :
 (defun mpcel-add-songs (art rest)
